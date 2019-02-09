55a484c490b6a01c358fe5b12ffa410fb7474164
Robin Schroer <sulami@peerwire.org>
2019-02-09 15:17:33 +0100

    Add some vague file searching

--- src/james/plugins/file_finder.clj
+++ src/james/plugins/file_finder.clj
@@ -1,16 +1,31 @@
-(ns james.plugins.file-finder)
+(ns james.plugins.file-finder
+  (:require [clojure.java.io :as io]))
+
+(def search-dirs
+  ["/Users/robinschroer/build/spielwiese/"])
 
 (defn file-finder-action
   [result]
   nil)
 
+(defn search-dir [query dir]
+  (->> dir
+       io/file
+       file-seq
+       rest))
+
+(defn format-result [result]
+  {:title (.getName result)
+   :subtitle (.getParent result)
+   :action nil
+   :plugin nil
+   :relevance 1}) ;; TODO actual values
+
 (defn file-finder-eval
   [input]
-  (repeat 3 {:title input
-             :subtitle ""
-             :action nil
-             :plugin nil
-             :relevance 0.2})) ;; Fake
+  (->> search-dirs
+       (mapcat (partial search-dir input))
+       (map format-result)))
 
 (def file-finder-plugin
   {:name "File finder"
